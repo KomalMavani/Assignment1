9a2b13e1cf091b842a44a627e0771b239c2a43a1
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2024-10-02 20:57:28 -0400

    Add comments

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const app = express();  //create object of express
 app.use(express.json()); 
 const port = 5000; // Assign port on which want to run application
 
+//Empty products object
 let datastore = {
     products: [],
 }
 
+//HTTP Get Request
 app.get('/getproducts',(req, res) => {
     res.json(datastore.products);
 })
 
+//HTTP Post Request
 app.post('/addproduct',(req, res) => {
     const product = req.body;
 
@@ -21,13 +24,7 @@ app.post('/addproduct',(req, res) => {
     }
 
     // Check if a product with the same id already exists
-    console.log(datastore.products);
-
-
     const existingProduct = datastore.products.find(p => p.id == product.id);
-
-    console.log(existingProduct);
-
     if (existingProduct) {
         return res.status(409).json({ error: 'Id already exists, please add unique!!' });
     }
@@ -36,10 +33,12 @@ app.post('/addproduct',(req, res) => {
     res.status(200).json(product);
 })
 
+//HTTP Delete Request
 app.delete('/deleteproduct/:id',(req, res) => {
     const product = req.params.id;
-    const productIndex = datastore.products.findIndex(p => p.id === product);
+    const productIndex = datastore.products.findIndex(p => p.id === product); // To find the product with requested product Id
 
+    // Check if a product exists or not
     if (productIndex === -1) {
         return res.status(404).json({ error: 'Product not found' });
     }
